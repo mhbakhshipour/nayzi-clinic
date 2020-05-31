06ed98999571bd7996d5124ca3a2729753524d4f
m.bakhshipour <m.bakhshipour@tiara.studio>
2020-05-31 19:43:26 +0430

    static files

--- nayzi/settings.py
+++ nayzi/settings.py
@@ -144,9 +144,7 @@ MEDIA_URL = '/media/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "statics")
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "statics")
 
 UPLOAD_DIRECTORIES = {
     'service_thumbnail': 'service_thumbnail',
